1fc1c7845f9a53682202e9f4273184dd0c189efb
Sai Manoj <saimanoj2826@gmail.com>
2022-10-02 19:09:10 +0530

    adding score descriptions

--- src/components/RuleRow.js
+++ src/components/RuleRow.js
@@ -4,14 +4,14 @@ import '../styling/RuleRow.css';
 class RuleRow extends Component {
   // FIXME: call doScore with parameters
   render() {
-    const { score, name, doScore } = this.props;
+    const { score, name, desc, doScore } = this.props;
     const disabled = score !== undefined;
     return (
       <tr
         className={`RuleRow RuleRow-${disabled ? 'disabled' : 'active'}`}
         onClick={disabled ? null : doScore}>
         <td className='RuleRow-name'>{name}</td>
-        <td className='RuleRow-score'>{score}</td>
+        <td className='RuleRow-score'>{disabled ? score : desc}</td>
       </tr>
     );
   }
