1dc70969422cb619ce6468f392b055b2c2a56c2d
Sai Manoj <saimanoj2826@gmail.com>
2022-10-02 12:48:54 +0530

    fix clicking dice

--- src/components/Die.js
+++ src/components/Die.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import '../styling/Die.css';
 
 class Die extends Component {
+  constructor(props) {
+    super(props);
+    this.handleDieClick = this.handleDieClick.bind(this);
+  }
+
+  handleDieClick() {
+    this.props.handleClick(this.props.idx);
+  }
+
   // FIXME: 2)call hadleClick with parameters
   render() {
     return (
       <button
         className='Die'
         style={{ backgroundColor: this.props.locked ? 'grey' : 'black' }}
-        onClick={this.props.handleClick}>
+        onClick={this.handleDieClick}>
         {this.props.val}
       </button>
     );
